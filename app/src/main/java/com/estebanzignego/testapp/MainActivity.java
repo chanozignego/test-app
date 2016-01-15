bec3bf1549e83b496f942bd10b57ca975049d23e
chanozignego <esteban.zignego@gmail.com>
2016-01-15 01:30:03 -0300

    add change main activity

--- app/src/main/java/com/estebanzignego/testapp/MainActivity.java
+++ app/src/main/java/com/estebanzignego/testapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.estebanzignego.testapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void accept(View view) {
+        Intent intent = new Intent(this, PaymentMethodsActivity.class);
+        startActivity(intent);
     }
 }
